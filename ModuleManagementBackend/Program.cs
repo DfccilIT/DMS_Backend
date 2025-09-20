03dfbbab41192293adbe5244d46e7307a8f88608
Web Team 2 <web_team2@dfccil.com>
2025-09-20 14:30:49 +0000

    changes as per naveen sir

--- ModuleManagementBackend/Program.cs
+++ ModuleManagementBackend/Program.cs
@@ -17,7 +17,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApiProjectServices(builder.Configuration);
 builder.Services.AddBusinessLayerServices(builder.Configuration);
 builder.Services.AddDataAccessLayerServices(builder.Configuration);
-
+app.MapGet("/health", () =>
+ {
+     return Results.Ok(new { status = "Healthy" });
+ });
+ app.MapGet("/", () => Results.Ok("API is running"));
 var app = builder.Build();
 app.ExceptionHandler();
 
